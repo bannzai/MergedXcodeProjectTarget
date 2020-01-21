fa864309944a25b367a5dbb8b4b636a736f5ce92
bannzai <kingkong999yhirose@gmail.com>
2020-01-21 20:07:15 +0900

    :recycle: Missing call method

--- Sources/MergedXcodeProjectTarget/main.swift
+++ Sources/MergedXcodeProjectTarget/main.swift
@@ -61,10 +61,10 @@ func main() {
         }
         
         
-        let sourceFiles = sourceTarget.context.fileRefs
-        let destinationFiles = destinationTarget.context.fileRefs
+        let sourceFiles = sourceTarget.buildPhases.flatMap { $0.files }
+        let destinationFiles = destinationTarget.buildPhases.flatMap { $0.files }
         
-        func filePathOrName(fileRef: PBX.FileReference) -> String {
+        func filePathOrName(fileRef: PBX.Reference) -> String {
             return fileRef.path ?? fileRef.name!
         }
         let fileExtension = extractArgument(key: "extension") ?? ".swift"
@@ -73,13 +73,13 @@ func main() {
             if destinationFiles.contains(where: { $0.id == sourceFile.id }) {
                 return
             }
-            if !destinationFiles.contains(where: { filePathOrName(fileRef: $0).hasSuffix(fileExtension) }) {
+            if !destinationFiles.contains(where: { filePathOrName(fileRef: $0.fileRef).hasSuffix(fileExtension) }) {
                 return
             }
-            if destinationFiles.contains(where: { ignoreFiles.contains(filePathOrName(fileRef: $0)) }) {
+            if destinationFiles.contains(where: { ignoreFiles.contains(filePathOrName(fileRef: $0.fileRef)) }) {
                 return
             }
-            project.appendFile(path: filePathOrName(fileRef: sourceFile), targetName: destinationTargetName)
+            project.appendFile(path: filePathOrName(fileRef: sourceFile.fileRef), targetName: destinationTargetName)
         }
         try project.write()
     } catch {
