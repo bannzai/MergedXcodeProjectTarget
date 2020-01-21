1171b2644544dbddbc5b2a46ea5a054cecddb662
bannzai <kingkong999yhirose@gmail.com>
2020-01-21 19:50:54 +0900

    :recycle: Fix extensions

--- Sources/MergedXcodeProjectTarget/main.swift
+++ Sources/MergedXcodeProjectTarget/main.swift
@@ -3,14 +3,14 @@ import XcodeProjectCore
 
 func usage() {
     print("""
-Usage: mxpt [subcommand] [options | --project PROJECT_PATH, --from SOURCE_TARGET_NAME --to DESTINATION_TARGET_NAME]
+Usage: mxpt [subcommand] [options | --project PROJECT_PATH, --from SOURCE_TARGET_NAME --to DESTINATION_TARGET_NAME, --extension=.swift, --ignoreFileNames=AppDelegate.swift,main.swift]
 
 mxpt help       Print usage mxpt
 mxpt merge      Execute merge from source target to destination target.
 """)
 }
 
-func extractArgument(key: String) -> String {
+func extractArgument(key: String) -> String? {
     let indices = ProcessInfo.processInfo.arguments[1...].indices
     for index in indices {
         let optionName = ProcessInfo.processInfo.arguments[index]
@@ -18,7 +18,14 @@ func extractArgument(key: String) -> String {
             return ProcessInfo.processInfo.arguments[index + 1]
         }
     }
-    fatalError("Can not find option \(key)")
+    return nil
+}
+
+func buildIgnoreFileList() -> [String] {
+    guard let names = extractArgument(key: "ignoreFileNames") else {
+        return []
+    }
+    return names.split(separator: ",").map { String($0) }
 }
 
 func main() {
@@ -32,16 +39,22 @@ func main() {
         fatalError("Could not allow option for \(subcommand). See `$ mxpt help`")
     }
     
-    let xcodeProjectUrl = ProcessInfo.processInfo.environment["MERGED_XCODE_PROJECT_TARGET_PATH"] ?? extractArgument(key: "--project")
+    guard let xcodeProjectUrl = ProcessInfo.processInfo.environment["MERGED_XCODE_PROJECT_TARGET_PATH"] ?? extractArgument(key: "--project") else {
+        fatalError("Required option for --project. See `$ mxpt help`")
+    }
     do {
         let project = try XcodeProject(
             xcodeprojectURL: URL(string: "file://" + xcodeProjectUrl)!
         )
 
-        let sourceTargetName = extractArgument(key: "--from")
-        let destinationTargetName = extractArgument(key: "--to")
+        guard let sourceTargetName = extractArgument(key: "--from") else {
+            fatalError("Required option for --from. See `$ mxpt help`")
+        }
+        guard let destinationTargetName = extractArgument(key: "--to") else{
+            fatalError("Required option for --to. See `$ mxpt help`")
+        }
         guard let sourceTarget = project.targets.first(where: { $0.name == sourceTargetName }) else {
-                fatalError("Can not find \(sourceTargetName)")
+            fatalError("Can not find \(sourceTargetName)")
         }
         guard let destinationTarget = project.targets.first(where: { $0.name == destinationTargetName }) else {
             fatalError("Can not find \(destinationTargetName)")
@@ -50,11 +63,22 @@ func main() {
         let sourceFiles = sourceTarget.buildPhases.flatMap { $0.files }
         let destinationFiles = destinationTarget.buildPhases.flatMap { $0.files }
         
+        func filePathOrName(fileRef: PBX.Reference) -> String {
+            return fileRef.path ?? fileRef.name!
+        }
+        let fileExtension = extractArgument(key: "extension") ?? ".swift"
+        let ignoreFiles = buildIgnoreFileList()
         sourceFiles.forEach { sourceFile in
             if destinationFiles.contains(where: { $0.id == sourceFile.id }) {
                 return
             }
-            project.appendFile(path: sourceFile.fileRef.path ?? sourceFile.fileRef.name!, targetName: destinationTargetName)
+            if destinationFiles.contains(where: { filePathOrName(fileRef: $0.fileRef).hasSuffix(fileExtension) }) {
+                return
+            }
+            if destinationFiles.contains(where: { ignoreFiles.contains(filePathOrName(fileRef: $0.fileRef)) }) {
+                return
+            }
+            project.appendFile(path: filePathOrName(fileRef: sourceFile.fileRef), targetName: destinationTargetName)
         }
         try project.write()
     } catch {
